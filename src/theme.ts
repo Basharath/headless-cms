2b29e7973a7727e67c9e5f20f7b52963933c232b
Basharath <Basharath.in@gmail.com>
2021-11-28 20:46:51 +0530

    Updates themes.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,16 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { red, blue } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: blue[500],
+      light: blue[300],
+      dark: blue[600],
     },
     secondary: {
-      main: '#19857b',
+      main: '#d81b60',
     },
     error: {
       main: red.A400,
