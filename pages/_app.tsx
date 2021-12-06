70f5ba7252dcceb02370966a1018f475fc7e06af
Basharath <Basharath.in@gmail.com>
2021-12-06 19:28:21 +0530

    Adds preloader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -32,6 +34,19 @@ export default function MyApp(props: MyAppProps) {
         // toast.error(err?.response?.data || err?.message);
       }
     })();
+
+    const start = () => NProgress.start();
+    const end = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
   }, []);
 
   return (
